fe9cf590f984dd227970f7f7f81ce51457330507
Anders Demant van der Weide <antra@antra.dk>
2022-07-30 17:51:30 +0200

    update example configuration

--- nordnet_configuration_example.py
+++ nordnet_configuration_example.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Nordnet user account credentials and accounts/portfolios names (choose yourself) and numbers.
 # To get account numbers go to https://www.nordnet.dk/transaktioner and change
 # between accounts. The number after "accid=" in the new URL is your account number.
 # If you have only one account, your account number is 1.
-user = 'myusername'
-password = 'mypassword'
+user = os.getenv('nordnet_username', 'myusername')
+password = os.getenv('nordnet_password', 'mypassword')
 accounts = {
     'account one': '1',
     'account three': '3'
@@ -21,9 +26,9 @@ currencies_filename = 'currencies.csv'
 # (identifier is 16256554)
 # All shares must have a name (whatever you like). To get prices they must
 # have a Nordnet identifier
-nordnet_sharelist = [
-	["Novo Nordisk B A/S", '16256554'],
-	["Tryg A/S", '16097566']
+sharelist = [
+    ["Novo Nordisk B A/S", '16256554'],
+    ["Tryg A/S", '16097566']
 ]
 # Start date (start of historical price period)
 prices_startdate = '2013-01-01'
